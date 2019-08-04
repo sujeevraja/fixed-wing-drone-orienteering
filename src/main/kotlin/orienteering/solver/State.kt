2e7c184d75123cd3c77eb4cc0de8cdaaddb877d4
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-08-04 07:34:27 -0700

    include #targets in strict dominance check
    Otherwise, the dominance proof can fail and cause the algorithm
    to remove a label that is part of the optimal solution.

--- src/main/kotlin/orienteering/solver/State.kt
+++ src/main/kotlin/orienteering/solver/State.kt
@@ -90,6 +90,12 @@ class State private constructor(
         }
 
         if (useVisitCondition) {
+            if (numTargetsVisited > other.numTargetsVisited) {
+                return false
+            }
+            if (!strict && numTargetsVisited < other.numTargetsVisited) {
+                strict = true
+            }
             for (i in 0 until visitedBits.size) {
                 // Following condition is satisfied when "this" visits a critical target and
                 // "other" does not. So, "this" does not dominate the "other".
