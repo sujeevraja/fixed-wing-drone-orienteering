296a7cb88a9270c4a208c93c70f278fcdfac721d
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-08-08 11:42:13 -0700

    add class to manage monitor actor state

--- src/main/kotlin/orienteering/solver/MonitorActor.kt
+++ src/main/kotlin/orienteering/solver/MonitorActor.kt
@@ -4,10 +4,11 @@ import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.actor
+import mu.KLogging
 import kotlin.coroutines.CoroutineContext
 
 sealed class MonitorActorMessage
-class AlgorithmStatus: MonitorActorMessage() {
+class AlgorithmStatus : MonitorActorMessage() {
     val branchingActorAvailable = CompletableDeferred<Boolean>()
     val branchingOngoing = CompletableDeferred<Boolean>()
     val openNodesAvailable = CompletableDeferred<Boolean>()
@@ -18,45 +19,53 @@ data class BranchingCompleted(val branchingActorId: Int) : MonitorActorMessage()
 data class BranchingStarted(val branchingActorId: Int) : MonitorActorMessage()
 object OpenNodesExist : MonitorActorMessage()
 object OpenNodesEmpty : MonitorActorMessage()
-object TerminateAlgorithm: MonitorActorMessage()
+object TerminateAlgorithm : MonitorActorMessage()
+
+class MonitorActorState(numBranchingActors: Int) {
+    private val branchingActorRunning = MutableList(numBranchingActors) { false }
+    private var openNodesExist = false
+    private var optimalityReached = false
+
+    fun handle(message: MonitorActorMessage) {
+        when (message) {
+            is AlgorithmStatus -> provideAlgorithmStatus(message)
+            is BranchingCompleted -> updateBranchingStatus(message.branchingActorId, busy = false)
+            is BranchingStarted -> updateBranchingStatus(message.branchingActorId, busy = true)
+            is OpenNodesExist -> markOpenNodesExist(true)
+            is OpenNodesEmpty -> markOpenNodesExist(false)
+            is TerminateAlgorithm -> optimalityReached = true
+        }
+    }
+
+    private fun provideAlgorithmStatus(algorithmStatus: AlgorithmStatus) {
+        logger.debug("branchingActorRunning: $branchingActorRunning")
+        logger.debug("openNodesExist: $openNodesExist")
+        logger.debug("optimalityReached: $optimalityReached")
+
+        algorithmStatus.branchingActorAvailable.complete(branchingActorRunning.any { !it })
+        algorithmStatus.branchingOngoing.complete(branchingActorRunning.any { it })
+        algorithmStatus.openNodesAvailable.complete(openNodesExist)
+        algorithmStatus.optimalityReached.complete(optimalityReached)
+    }
+
+    private fun updateBranchingStatus(branchingActorId: Int, busy: Boolean) {
+        branchingActorRunning[branchingActorId] = busy
+        logger.debug("branchingActorRunning $branchingActorRunning")
+    }
+
+    private fun markOpenNodesExist(exist: Boolean) {
+        openNodesExist = exist
+        logger.debug("open nodes exist: $openNodesExist")
+    }
+
+    companion object : KLogging()
+}
 
 @ObsoleteCoroutinesApi
 fun CoroutineScope.monitorActor(context: CoroutineContext, numBranchingActors: Int) =
     actor<MonitorActorMessage>(context = context) {
-        val branchingActorRunning = MutableList(numBranchingActors) { false }
-        var openNodesExist = false
-        var optimalityReached = false
-
+        val state = MonitorActorState(numBranchingActors)
         for (message in channel) {
-            when (message) {
-                is AlgorithmStatus -> {
-                    println("monitorActor ${Thread.currentThread().name}: branchingActorRunning $branchingActorRunning")
-                    println("monitorActor ${Thread.currentThread().name}: openNodesExist $openNodesExist")
-                    println("monitorActor ${Thread.currentThread().name}: optimalityReached $optimalityReached")
-                    message.branchingActorAvailable.complete(branchingActorRunning.any { !it })
-                    message.branchingOngoing.complete(branchingActorRunning.any { it })
-                    message.openNodesAvailable.complete(openNodesExist)
-                    message.optimalityReached.complete(optimalityReached)
-                }
-                is BranchingCompleted -> {
-                    println("monitorActor ${Thread.currentThread().name}: received BranchingCompleted from ${message.branchingActorId}")
-                    branchingActorRunning[message.branchingActorId] = false
-                    println("monitorActor ${Thread.currentThread().name}: branchingActorRunning $branchingActorRunning")
-                }
-                is BranchingStarted -> {
-                    println("monitorActor ${Thread.currentThread().name}: received BranchingStarted from ${message.branchingActorId}")
-                    branchingActorRunning[message.branchingActorId] = true
-                    println("monitorActor ${Thread.currentThread().name}: branchingActorRunning $branchingActorRunning")
-                }
-                is OpenNodesExist -> {
-                    println("monitorActor ${Thread.currentThread().name}: received OpenNodesExist")
-                    openNodesExist = true
-                }
-                is OpenNodesEmpty -> {
-                    println("monitorActor ${Thread.currentThread().name}: received OpenNodesEmpty")
-                    openNodesExist = false
-                }
-                is TerminateAlgorithm -> optimalityReached = true
-            }
+            state.handle(message)
         }
     }
