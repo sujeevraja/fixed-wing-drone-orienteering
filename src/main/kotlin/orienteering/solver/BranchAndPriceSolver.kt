be0cee9f68c2176e3d55fd5745a3539df701d0e2
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-08-12 07:51:56 -0700

    add some documentation

--- src/main/kotlin/orienteering/solver/BranchAndPriceSolver.kt
+++ src/main/kotlin/orienteering/solver/BranchAndPriceSolver.kt
@@ -28,8 +28,17 @@ class BranchAndPriceSolver(private val instance: Instance) {
             val solvedNodes = Channel<Node>()
             val solution = CompletableDeferred<BranchAndPriceSolution>()
 
+            // Prepare a set of coroutines to consume nodes from the unsolvedNodes channel, solve
+            // them and send back the solved nodes to the solvedNodes channel. Each coroutine is
+            // created by the "launch" command and remains suspended at the for loop line, i.e.
+            // waiting for the iterator of the unsolvedNodes channel to release a node.
             repeat(numSolvers) {
                 launch {
+                    // This CPLEX object will be created just once and be re-used for all nodes
+                    // released by the iterator in the loop below. This is because the scope
+                    // (together with the CPLEX object and the solveNode() function call will
+                    // persist as long as the for loop is suspended. We will go out of scope only
+                    // when the unsolvedNodes channel is closed.
                     val cplex = IloCplex()
                     for (node in unsolvedNodes) {
                         solveNode(cplex, node, solvedNodes)
@@ -37,11 +46,23 @@ class BranchAndPriceSolver(private val instance: Instance) {
                 }
             }
 
+            // Solve the root node by sending it to the unsolvedNodes channel and consuming it
+            // immediately from the solvedNodes channel to collect root nodes bounds and an initial
+            // solution.
             val solvedRootNode = solveRootNode(unsolvedNodes, solvedNodes)
+
+            // Prepare to send the root node back into the solvedNodes channel so that the node
+            // processor can branch on it if needed.
             launch {
                 logger.info("sending solvedRootNode $solvedRootNode back into solvedNodes")
                 solvedNodes.send(solvedRootNode)
             }
+
+            // Prepare a coroutine to consume nodes from the solvedNodes channel and use their
+            // LP/MIP objective and solution values to update global bounds, prune them or branch
+            // on them. If branched, new unsolved nodes will be stored in the node processor's
+            // open node queue and released to the unsolvedNodes channel whenever solvers are
+            // available.
             launch {
                 val nodeProcessor =
                     NodeProcessor(solvedRootNode, instance, numSolvers, solution)
@@ -51,6 +72,16 @@ class BranchAndPriceSolver(private val instance: Instance) {
                 }
             }
 
+            // The data pipeline we have set up is:
+            // 1. Consume node from the solvedNodes channel with the node processor.
+            // 2. If the algorithm can terminate, complete the deferred "solution" variable with a
+            //    value.
+            // 3. Otherwise, the node processor should still have some open nodes. Send these nodes
+            //    to the unsolvedNodes channel whenever there are solvers available.
+            // 4. Consume nodes from the unsolved nodes channel with the solver pool.
+            // 5. Send back solved nodes to the solvedNodes channel.
+            // As explained above, the only termination condition now is that the "solution"
+            // completes. We simply wait for it here.
             finalSolution = solution.await()
             coroutineContext.cancelChildren()
         }
