76b7671a0349d8e120c8c356c7f2824b49028e66
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-09-15 10:04:19 -0700

    collect instance queries in single file

--- final-results/queries_for_instances.sql
+++ final-results/queries_for_instances.sql
@@ -1,10 +1,12 @@
-----------------------------------------------------------------------------
 -- The following query selects runs used for comparison tables in the paper.
--- The query gives 59 instance/discretization pairs.
--- We do non-exhaustive runs using these pairs.
-----------------------------------------------------------------------------
 select instance_path, instance_name, number_of_discretizations from exhaustive where
     optimality_reached = "True" and
     cast(solution_time_in_seconds as decimal(16,2)) >= 60.0 and
     ((instance_path like "%_32_%") or (instance_path like "%_33_%"))
+    order by instance_path, instance_name, number_of_discretizations
+
+-- This query selects instances to do a single vs multi threading comparison of I-DSSR.
+select instance_path, instance_name, number_of_discretizations from exhaustive where
+    cast(number_of_nodes_solved as integer) > 1 and
+    optimality_reached = "True"
     order by instance_path, instance_name, number_of_discretizations
