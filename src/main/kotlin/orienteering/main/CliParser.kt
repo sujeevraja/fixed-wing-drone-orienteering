395cc6f51d19f3a1b288bec3f909e5c45d31300b
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2021-07-27 07:19:21 -0700

    Bang for buck comparison (#5)
    * make bang-for-buck selection configurable
    
    If bang-for-buck is turned off, select labels with least reduced cost.
    
    * finish bang-for-buck script

--- src/main/kotlin/orienteering/main/CliParser.kt
+++ src/main/kotlin/orienteering/main/CliParser.kt
@@ -25,8 +25,7 @@ class CliParser : CliktCommand() {
     val instanceName: String by option(
         "-n",
         help = "instance name"
-    )
-        .default("p3.2.k.txt")
+    ).default("p3.2.k.txt")
 
     /**
      * path to folder with instance file
@@ -34,74 +33,65 @@ class CliParser : CliktCommand() {
     val instancePath: String by option(
         "-p",
         help = "instance path"
-    )
-        .default("./data/Set_33_234/")
-        .validate {
-            require(File(instancePath + instanceName).exists()) {
-                "file does not exist, check the file path and name"
-            }
+    ).default("./data/Set_33_234/").validate {
+        require(File(instancePath + instanceName).exists()) {
+            "file does not exist, check the file path and name"
         }
+    }
 
     val outputPath: String by option(
         "-o",
         help = "path to file with output KPIs"
-    )
-        .default("./logs/results.yaml")
-        .validate {
-            require(it.length > 5 && it.endsWith(".yaml")) {
-                "output path should end with a non-empty file name and .yaml extension"
-            }
+    ).default("./logs/results.yaml").validate {
+        require(it.length > 5 && it.endsWith(".yaml")) {
+            "output path should end with a non-empty file name and .yaml extension"
         }
+    }
 
     val algorithm: Int by option(
         "-a",
         help = "use branch-and-cut (1) or branch-and-price (2)"
-    )
-        .int().default(2).validate {
-            require(it in 1 until 3) {
-                "algorithm option can be in the set {1, 2}"
-            }
+    ).int().default(2).validate {
+        require(it in 1 until 3) {
+            "algorithm option can be in the set {1, 2}"
         }
+    }
 
     val numReducedCostColumns: Int by option(
         "-c",
         help = "limit on number of reduced cost columns to collect during pricing"
-    )
-        .int().default(500).validate {
-            require(it >= 1) {
-                "limit should be at least 1"
-            }
+    ).int().default(500).validate {
+        require(it >= 1) {
+            "limit should be at least 1"
         }
+    }
 
     val numDiscretizations: Int by option(
         "-d",
         help = "number of discretizations of the heading angle"
-    )
-        .int().default(2).validate {
-            require(it >= 1) {
-                "number of discretizations has to be >= 1 and integral"
-            }
+    ).int().default(2).validate {
+        require(it >= 1) {
+            "number of discretizations has to be >= 1 and integral"
         }
+    }
 
     val useInterleavedSearch: Int by option(
         "-i",
-        help = "use inter-leaved search (1) or simple search (0)"
-    )
-        .int().default(1).validate {
-            require(it == 1 || it == 0) {
-                "should be 1 or 0"
-            }
+        help = "use interleaved search (1) or simple search (0)"
+    ).int().default(1).validate {
+        require(it == 1 || it == 0) {
+            "should be 1 or 0"
         }
+    }
 
     val turnRadius: Double by option(
         "-r",
         help = "turn radius of the vehicle"
-    )
-        .double().default(1.0).validate {
-            require(it > 0.0) {
-                "turn radius has to be a positive number"
-            }
+    ).double().default(1.0).validate {
+        require(it > 0.0) {
+            "turn radius has to be a positive number"
         }
+    }
 
     val relaxDominanceRules: Int by option(
         "-rd",
@@ -115,35 +105,40 @@ class CliParser : CliktCommand() {
     val numSolverCoroutines: Int by option(
         "-s",
         help = "number of concurrent solves"
-    )
-        .int().default(8).validate {
-            require(it > 0) {
-                "number should be a strictly positive integer"
-            }
+    ).int().default(8).validate {
+        require(it > 0) {
+            "number should be a strictly positive integer"
         }
+    }
 
     val timeLimitInSeconds: Int by option(
         "-t",
         help = "time limit in seconds"
-    )
-        .int().default(3600).validate {
-            require(it > 0) {
-                "time limit should be a strictly positive integer"
-            }
+    ).int().default(3600).validate {
+        require(it > 0) {
+            "time limit should be a strictly positive integer"
         }
+    }
 
     val useNumTargetsForDominance: Int by option(
         "-u",
         help = "use number of targets in dominance check"
-    )
-        .int().default(0).validate {
-            require(it == 1 || it == 0) {
-                "should be 1 or 0"
-            }
+    ).int().default(0).validate {
+        require(it == 1 || it == 0) {
+            "should be 1 or 0"
         }
+    }
+
+    val useBangForBuck: Int by option(
+        "-b",
+        help = "use bang for buck (1) or least reduced cost (0) to select labels"
+    ).int().default(1).validate {
+        require(it == 1 || it == 0) {
+            "should be 1 or 0"
+        }
+    }
 
     override fun run() {
         logger.debug("reading command line arguments...")
     }
 }
-
