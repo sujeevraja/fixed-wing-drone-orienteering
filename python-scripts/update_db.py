e77a5ceb7a2b404b712a28fa62e1b4e533ba30fa
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2021-08-06 11:45:44 -0700

    prepare DB with latest exhaustive results

--- python-scripts/update_db.py
+++ python-scripts/update_db.py
@@ -5,20 +5,10 @@ import logging
 import operator
 import os
 import sqlite3
+import typing
 import yaml
 
 log = logging.getLogger(__name__)
-
-
-class Config(object):
-    """Class that holds global parameters."""
-
-    def __init__(self):
-        folder_path = os.path.dirname(os.path.realpath(__file__))
-        self.base_path = os.path.abspath(os.path.join(folder_path, '..'))
-        self.results_path = os.path.join(self.base_path, 'results')
-        self.db_path = os.path.join(self.base_path, 'final-results', 'results.db')
-        self.table_name = 'search_comparison'
 
 
 class ScriptException(Exception):
@@ -30,6 +20,12 @@ class ScriptException(Exception):
 
     def __repr__(self):
         return repr(self.value)
+
+
+class Config(typing.NamedTuple):
+    results_path: str
+    db_path: str
+    table_name: str
 
 
 def table_exists(cursor, name):
@@ -49,6 +45,18 @@ def create_table(cursor, name, fields):
             f"""CREATE TABLE {name} ({field_str})""")
 
 
+def get_table_fields(results_path: str) -> typing.List[str]:
+    for f in os.listdir(results_path):
+        if not f.endswith(".yaml"):
+            continue
+
+        with open(os.path.join(results_path, f), 'r') as fin:
+            result_dict = yaml.load(fin, Loader=yaml.FullLoader)
+            return list(result_dict.keys())
+
+    raise ScriptException(f"no results found at {results_path}")
+
+
 class Controller:
     """class that manages the functionality of the entire script."""
 
@@ -64,31 +72,18 @@ class Controller:
 
         self._write_results_table()
 
-        if self.config.table_name == "bangforbuck":
-            self._write_args_table()
+        # if self.config.table_name == "bangforbuck":
+        #     self._write_args_table()
 
         self._connection.commit()
         self._cursor.close()
         self._connection.close()
         log.info("result addition completed")
-    
+
     def _write_results_table(self):
-        table_created = False
-        for f in os.listdir(self.config.results_path):
-            if not f.endswith(".yaml"):
-                continue
-
-            with open(os.path.join(self.config.results_path, f), 'r') as fin:
-                result_dict = yaml.load(fin, Loader=yaml.FullLoader)
-                col_names = list(result_dict.keys()) + [self._result_id_col]
-                col_names.sort()
-                create_table(self._cursor, self.config.table_name, col_names)
-                table_created = True
-            
-            break
-
-        if not table_created:
-            raise ScriptException(f"no yaml file found in results folder")
+        fields = get_table_fields(self.config.results_path)
+        create_table(self._cursor, self.config.table_name,
+                     sorted(fields + [self._result_id_col]))
 
         for f in os.listdir(self.config.results_path):
             if f.endswith(".yaml"):
@@ -96,6 +91,21 @@ class Controller:
                 self._add_results_to_table(fpath)
                 log.info(f"added results for {f}")
 
+    def _add_results_to_table(self, fpath):
+        with open(fpath, "r") as f_result:
+            name = os.path.basename(fpath.strip()).split(".")[0]
+            id = int(name.split("_")[1])
+            result_dict = yaml.load(f_result, Loader=yaml.FullLoader)
+            keys_and_vals = [(k, v) for (k, v) in result_dict.items()] + \
+                [(self._result_id_col, id)]
+            keys_and_vals.sort(key=operator.itemgetter(0))
+            _, values = zip(*keys_and_vals)
+            values = [f"'{v}'" for v in values]
+
+            cmd = f"""
+                INSERT INTO {self.config.table_name}
+                VALUES ({",".join(values)})"""
+            self._cursor.execute(cmd)
 
     def _write_args_table(self):
         table_name = self.config.table_name + "_args"
@@ -127,43 +137,27 @@ class Controller:
                     VALUES ({",".join(row)})"""
                 self._cursor.execute(insert_cmd)
 
-    def _add_results_to_table(self, fpath):
-        with open(fpath, "r") as f_result:
-            name = os.path.basename(fpath.strip()).split(".")[0]
-            id = int(name.split("_")[1])
-            result_dict = yaml.load(f_result, Loader=yaml.FullLoader)
-            keys_and_vals = [(k, v) for (k, v) in result_dict.items()] + \
-                [(self._result_id_col, id)]
-            keys_and_vals.sort(key=operator.itemgetter(0))
-            _, values = zip(*keys_and_vals)
-            values = [f"'{v}'" for v in values]
 
-            cmd = f"""
-                INSERT INTO {self.config.table_name}
-                VALUES ({",".join(values)})"""
-            self._cursor.execute(cmd)
+def handle_command_line() -> Config:
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    db_path = os.path.join(base_path, "final-results-v2", "results.db")
+    parser.add_argument("-d", "--db_path", type=str,
+                        help="path to SQLite DB file", default=db_path)
 
-def handle_command_line():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-r", "--resultspath", type=str,
+    results_path = os.path.join(base_path, "final-results-v2", "results")
+    parser.add_argument("-r", "--results_path", type=str,
                         help="path to folder with results",
-                        default="../results")
-    parser.add_argument("-t", "--tablename", type=str,
+                        default=results_path)
+
+    parser.add_argument("-t", "--table_name", type=str,
                         help="name of table to add results to",
-                        default="bangforbuck")
+                        default="exhaustive")
 
     args = parser.parse_args()
-    config = Config()
-
-    if args.resultspath:
-        config.results_path = args.resultspath
-
-    if args.tablename:
-        config.table_name = args.tablename
-
-    return config
+    return Config(**vars(args))
 
 
 def main():
