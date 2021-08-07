bbf182f40df9c9be88fc8b390b0e4ba8d7cd34cb
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2021-08-06 20:18:45 -0700

    update query

--- queries/instance_queries.sql
+++ queries/instance_queries.sql
@@ -1,6 +1,7 @@
 -- This query selects instances with which 1-thread DSSR and 1-thread I-DSSR will be compared.
 select instance_path, instance_name, number_of_discretizations from exhaustive where
     optimality_reached = "True"
+    and cast(solution_time_in_seconds as decimal(16,2)) >= 60.0
     and cast(solution_time_in_seconds as decimal(16,2)) < 3600.0
     and ((instance_path like "%_32_%") or (instance_path like "%_33_%"))
     order by instance_path, instance_name, number_of_discretizations
