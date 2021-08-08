9c974741455d06ff0c89c269a44cb59ba1928648
kaarthiksundar <kaarthik01sundar@gmail.com>
2021-08-08 09:03:46 -0600

    Adds exhuastive zip

--- threading/submit-batch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-lines=`wc -l < $1` 
-
-echo "running: sbatch --output="output/slurm-%A_%a.out" --array=1-$lines%500 slurm-batch-job.sh $1"
-
-sbatch --output="output/slurm-%A_%a.out" --array=1-$lines%500 slurm-batch-job.sh $1
-
